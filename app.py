531381c46f973e39f6fec81f65f50be47f8315e4
AndryanBIma1974 <musthaqahamedj25bm@srishakthi.ac.in>
2026-08-20 12:32:21 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1e62QS7p5-g2b9YOgv0osqXN-FH3NPeLK
 """
 
-!pip install streamlit
 import streamlit as st
 import pandas as pd
 import numpy as np
